48060eabf86f52fd3634cfa7bb3d200e21e724ea
Nakamoto <nanomalloc@gmail.com>
2017-09-25 22:44:50 +0800

    three modes

---
+++ memento's/top-navbar.js
@@ -0,0 +1,43 @@
+var topNavbarInnerHTML = `<a class="navbar-brand" href="home.html" id="caption">Solemia</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="navbar-nav">
+    <div class="dropdown" id='#lsr-dropdown'>
+      <a class="nav-item nav-link" data-toggle="dropdown" target="_blank" id="lsr">Leisure
+              <i class="fa fa-caret-down"></i>
+      </a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" target="_blank">Book</a>
+        <a class="dropdown-item" target="_blank">Movie</a>
+        <a class="dropdown-item" target="_blank">Music</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" target="_blank">Photograghy</a>
+        <a class="dropdown-item" target="_blank">Travel jounal</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" target="_blank">Gourment</a>
+      </div>
+    </div>
+    <div class="dropdown">
+      <a class="nav-item nav-link" target="_blank" data-toggle="dropdown" id="rfrc">Reference
+                <i class="fa fa-caret-down"></i>
+      </a>
+      <div class="dropdown-menu">
+        <h6 class="dropdown-header">Language</h6>
+        <a class="dropdown-item" href="#">English</a>
+        <a class="dropdown-item" href="#">Français</a>
+        <a class="dropdown-item " href="verb-table.html">Deutsch</a>
+        <div class="dropdown-divider"></div>
+        <h6 class="dropdown-header">Programming</h6>
+        <a class="dropdown-item" href="#">JavaScript</a>
+        <a class="dropdown-item" href="#">Web-design</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Other</a>
+      </div>
+    </div>
+    <a class="nav-item nav-link" href="index-bootstrap.html" target="_blank" id="mmt">Memento's</a>
+    <a class="nav-item nav-link" href="#" target="_blank">About</a>
+    <a class="nav-item nav-link disabled" href="#">More</a>
+  </div>
+</div>`
--- memento's/verb-table.js
+++ memento's/verb-table.js
@@ -1,33 +1,135 @@
-$(document).ready(function(){
-  for(let verb in verbs){
-    let tr = $('<tr/>').appendTo($('tbody'));   
-    let inf = $('<td/>').addClass('infinitive').attr('id','w-'+verbs[verb]["infinitive"]).text(verbs[verb]["infinitive"]).appendTo(tr); 
-    let past = $('<td/>').addClass('pastTense').text(verbs[verb]["pastTense"]).appendTo(tr);
-    let perfect = $('<td/>').addClass('presentPerfect').text(verbs[verb]["presentPerfect"]).appendTo(tr);  
+var modes = ['preterite', 'perfect', 'both', 'off']
+var map = ['.pastTense', '.presentPerfect', 'both', 'off']
+var modeFlag = 3;
+
+$(document).ready(function () {
+  $('#topNav').html(topNavbarInnerHTML);
+  $('#rfrc').addClass('active');
+  refreshTable();
+})
+
+var refreshTable = function () {
+  $('tbody').empty();
+  for (let verb in verbs) {
+    let tr = $('<tr/>').addClass('animated').appendTo($('tbody'));
+    let inf = $('<td/>').addClass('infinitive unchecked').text(verbs[verb]["infinitive"]).appendTo(tr);
+    let past = $('<td/>').addClass('pastTense animated').attr('spellcheck', 'false').text(verbs[verb]["pastTense"]).appendTo(tr);
+    let perfect = $('<td/>').addClass('presentPerfect animated').attr('spellcheck', 'false').text(verbs[verb]["presentPerfect"]).appendTo(tr);
+  }
+  $('tr').last('').attr('id','bottom')
+}
+
+$('#searchBtn').click(function () {
+  var selection = window.getSelection().toString()
+  if (selection.length == 0) {
+    $('#mdl').modal();
+    $('#mdl').find('.modal-body p').text('Please select a word first');
+  } else {
+    let url = 'https://en.pons.com/translate?q=' + selection + '&l=deen&in=de&lf=de';
+    window.open(url, '_blank')
+  }
+})
+
+$('#quizBtn').click(function () {
+  modeFlag = (modeFlag + 1) % 4
+  modeFlag == 0 && $('#counterBtn').slideToggle();
+  $this = $(this)
+  $this.find('span').text(modes[modeFlag])
+  modeFlag == 3 && $('#counterBtn').slideToggle();
+  setMode();
+})
+
+var setMode = function () {
+  modeFlag == 2 || refreshTable()
+  updateNum();
+  modeFlag == 2 && $(map[0]).each(function (el) {
+    $(this).text(' ').addClass('editable')
+  })
+  modeFlag < 2 && clearAndFill()
+}
+
+var clearAndFill = function () {
+  $(map[modeFlag]).each(function (el) {
+    $(this).text('').toggleClass('editable')
+  })
+  $(map[!modeFlag]).each(function (el) {
+    let verb = getVerb($(this))
+    let conjugation = ''
+    conjugation = modeFlag == 0 ? perfect(verb) : preterite(verb)
+    $(this).text(conjugation)
+  })
+}
+
+
+$('table').on('keyup', 'td.pastTense', function (event) {
+  switch (event.keyCode) {
+    case 13: //enter
+      let $this = $(this)
+      if (check($this)) {
+        correctAnimate($this)
+        modeFlag==2 ? $this.siblings('.presentPerfect').focus() : $this.parent().next().find('.pastTense').focus()
+      }
+      break;
+    case 27: $this.blur(); break;
+    default: break;
+  }
+})
+
+$('table').on('keyup', 'td.presentPerfect', function (event) {
+  switch (event.keyCode) {
+    case 13: //enter
+      let $this = $(this)
+      if (check($this)) {
+        correctAnimate($this)
+        $this.siblings('.infinitive').removeClass('unchecked').parent().fadeOut(500, function () {
+          updateNum()
+          $this.parent().next().find('.pastTense').focus()
+        })
+      }
+      break;
+    case 27: $this.blur(); break;
+    default: break;
   }
 })
 
 
-$('#searchBtn').click(function(){
-  var selection = window.getSelection().toString()
-  window.location = 'https://en.pons.com/translate?q='+selection+'&l=deen&in=de&lf=de'
-})
+var check = function ($this) {
+  let input = $this.text()
+  let verb = getVerb($this)
+  let answer = $this.hasClass('pastTense') ? preterite(verb) : perfect(verb)
+  if (input == answer) return 1
+  else return 0
+}
 
-$('#quizBtn').click(function(){
-  $('.pastTense').each(function(el){
-    $(this).text('').addClass('editable');
-  })
-})
+var correctAnimate = function (obj) {
+  jQuery(obj).animate({
+    color: '#46B29D',
+    backgroundColor: jQuery.Color('rgba(70,178,157,0.1)')
+  }, 300)
+}
 
-$('#jumpTopBtn').click(function(){
-  $('thead').addClass('bounce'); 
-  var t =setTimeout(function() {
-    $('thead').removeClass('bounce'); 
-  }, 1000);  
+var preterite = function (verb) {
+  return verbs[verb]["pastTense"]
+}
+var perfect = function (verb) {
+  return verbs[verb]["presentPerfect"]
+}
+var getVerb = function (obj) {
+  return obj.siblings('.infinitive').text()
+}
+var updateNum = function () {
+  $('#counter').text($('.unchecked').length);
+}
+
+$('#jumpTopBtn').click(function () {
+  $('thead').addClass('bounce');
+  var t = setTimeout(function () {
+    $('thead').removeClass('bounce');
+  }, 1000);
 })
-$('#jumpBottomBtn').click(function(){
-  $('#bottom').addClass('bounce'); 
-  var t =setTimeout(function() {
-    $('#bottom').removeClass('bounce'); 
-  }, 1000);  
+$('#jumpBottomBtn').click(function () {
+  $('#bottom').addClass('bounce');
+  var t = setTimeout(function () {
+    $('#bottom').removeClass('bounce');
+  }, 1000);
 })
